66dec9b454fe5745ce1d573f39c5139f348590b6
nufeng1999 <18478162@21cn.com>
2022-01-11 17:17:24 +0800

    move plugins

---
+++ jupyter_MyMake_kernel/plugins/fluttercommand.py
@@ -0,0 +1,56 @@
+from typing import Dict, Tuple, Sequence,List
+from plugins.ISpecialID import IStag,IDtag,IBtag,ITag
+import os
+class MyFluttercommand(IStag):
+    kobj=None
+    def getName(self) -> str:
+        # self.kobj._write_to_stdout("setKernelobj setKernelobj setKernelobj\n")
+        
+        return 'MyFluttercommand'
+    def getAuthor(self) -> str:
+        return 'Author'
+    def getIntroduction(self) -> str:
+        return 'MyFluttercommand'
+    def getPriority(self)->int:
+        return 0
+    def getExcludeID(self)->List[str]:
+        return []
+    def getIDSptag(self) -> List[str]:
+        return ['fluttercmd']
+    def setKernelobj(self,obj):
+        self.kobj=obj
+        # self.kobj._write_to_stdout("setKernelobj setKernelobj setKernelobj\n")
+        return
+    def on_shutdown(self, restart):
+        return
+    def on_ISpCodescanning(self,key, value,magics,line) -> str:
+        # self.kobj._write_to_stdout(line+" on_ISpCodescanning\n")
+        self.kobj.addkey2dict(magics,'fluttercmd')
+        return self.commandhander(self,key, value,magics,line)
+    ##在代码预处理前扫描代码时调用    
+    def on_Codescanning(self,magics,code)->Tuple[bool,str]:
+        pass
+        return False,code
+    ##生成文件时调用
+    def on_before_buildfile(self,code,magics)->Tuple[bool,str]:
+        return False,''
+    def on_after_buildfile(self,returncode,srcfile,magics)->bool:
+        return False
+    def on_before_compile(self,code,magics)->Tuple[bool,str]:
+        return False,''
+    def on_after_compile(self,returncode,binfile,magics)->bool:
+        return False
+    def on_before_exec(self,code,magics)->Tuple[bool,str]:
+        return False,''
+    def on_after_exec(self,returncode,srcfile,magics)->bool:
+        return False
+    def on_after_completion(self,returncode,execfile,magics)->bool:
+        return False
+    def commandhander(self,key, value,magics,line):
+        magics['fluttercmd'] = value.strip()
+        if len(magics['fluttercmd'])>0:
+            env=self.kobj.get_magicsSvalue(magics,'env')
+            if len(env)<1:env=None
+            self.kobj.do_Py_command(magics['fluttercmd'],env=env,magics=magics)
+        return ''
+    
--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-from setuptools import setup
-
-setup(name='jupyter_MyMake_kernel',
+#!/usr/bin/env python
+# coding: utf-8
+with open("README.md", "r") as f:
+	long_description = f.read()
+import setuptools
+setuptools.setup(name='jupyter_MyMake_kernel',
       vemakefileion='0.0.1',
       description='Minimalistic Make kernel for Jupyter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
       author='nufeng',
       author_email='18478162@qq.com',
       license='MIT',
-      classifiemakefile=[
-          'License :: OSI Approved :: MIT License',
-      ],
       url='https://github.com/nufeng1999/jupyter-MyMake-kernel/',
-      download_url='https://github.com/nufeng1999/jupyter-MyMake-kernel/tarball/0.0.1',
-      packages=['jupyter_MyMake_kernel'],
+      download_url='https://github.com/nufeng1999/jupyter-MyMake-kernel/releases/tag/0.0.1',
+    packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
       scripts=['jupyter_MyMake_kernel/install_MyMake_kernel'],
       keywords=['jupyter', 'notebook', 'kernel', 'make','makefile'],
       include_package_data=True
